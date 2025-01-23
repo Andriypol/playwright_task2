da6a549cd17ce8f6e4240f36465fbb75b1558d3c
turtulian <turtulian@gmail.com>
2025-01-23 16:23:45 +0200

    deleted list reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['list'],
     ['allure-playwright'],
   ],
   use: {
