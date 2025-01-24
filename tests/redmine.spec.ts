8fbf56d17380b3d598aa6ddb3e87922f285e8f31
turtulian <turtulian@gmail.com>
2025-01-24 11:35:09 +0200

    changed Registration test to correct assertion text

--- tests/redmine.spec.ts
+++ tests/redmine.spec.ts
@@ -54,7 +54,7 @@ test.describe('Redmine Tests', () => {
 
     const errorAlert = page.locator('#errorExplanation');
     await expect(page).toHaveURL(/account\/register/);
-    await expect(errorAlert).toContainText('Email is invaliii');
+    await expect(errorAlert).toContainText('Email is invalid');
   });
 
   test('Validate main menu navigation links on homepage', async ({ page, homePage }) => {
